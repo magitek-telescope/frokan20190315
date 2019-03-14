05a4c23b554ad11803b21ac3880ccc89a583f8c9
HANATANI Takuma <mail@potato4d.me>
2019-03-15 03:37:31 +0900

    feat: Implement splash screen example

--- splash-screen/src/App.vue
+++ splash-screen/src/App.vue
@@ -1,14 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <SplashScreen @hide="onLoaded" v-if="!isLaunched" />
+    <div id="content" v-else>
+      <div class="container">
+        <div style="margin-bottom: 60px; text-align: center">
+          <img
+            src="https://elevenback.jp/_nuxt/img/5e1feb8.svg"
+            width="80"
+            alt
+          />
+        </div>
+        <el-row>
+          <el-col
+            :span="7"
+            v-for="(o, index) in 12"
+            :key="o"
+            :offset="index % 3 ? 1 : 0"
+            :style="{ marginBottom: '42px' }"
+          >
+            <el-card :body-style="{ padding: '0px' }">
+              <div style="height: 128px;padding: 32px 0; text-align: center">
+                <img src="./assets/logo.png" width="64" class="image" />
+              </div>
+              <div style="padding: 16px 16px 32px;">
+                <span>Item {{ o }}</span>
+              </div>
+            </el-card>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import SplashScreen from './components/SplashScreen.vue'
 
 export default Vue.extend({
-  name: 'app'
+  name: 'app',
+  components: {
+    SplashScreen
+  },
+  data() {
+    return {
+      isLaunched: false
+    }
+  },
+  methods: {
+    onLoaded() {
+      this.isLaunched = true
+    }
+  }
 })
 </script>
 
@@ -19,6 +62,26 @@ export default Vue.extend({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#content {
+  margin: 60px 0 0;
+  animation: slideIn 0.3s ease-out forwards;
+}
+
+.container {
+  width: 1024px;
+  margin: 0 auto;
+}
+
+@keyframes slideIn {
+  0% {
+    opacity: 0;
+    transform: translateY(10px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0px);
+  }
 }
 </style>
---
+++ splash-screen/src/components/SplashScreen.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="splash-launched">
+    <div>
+      <img src="https://elevenback.jp/_nuxt/img/5e1feb8.svg" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  mounted() {
+    setTimeout(() => {
+      this.$emit('hide')
+    }, 900)
+  }
+})
+</script>
+
+<style scoped>
+#splash-launched {
+  align-items: center;
+  justify-content: center;
+}
+
+#splash-launched img {
+  animation: slideOut 0.4s 0.5s ease-out forwards;
+}
+
+@keyframes slideOut {
+  0% {
+    transform: translateY(0px);
+    opacity: 1;
+  }
+  100% {
+    transform: translateY(10px);
+    opacity: 0;
+  }
+}
+</style>
