4a83a72b6177c497a54515a1c972781d01208ea9
HANATANI Takuma <mail@potato4d.me>
2019-03-15 00:25:02 +0900

    chore: format with prettier

--- splash-screen/src/App.vue
+++ splash-screen/src/App.vue
@@ -1,20 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-});
+  name: 'app'
+})
 </script>
 
 <style>
